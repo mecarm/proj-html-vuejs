e1fe43959ba644bbeef6d97a10be98776b4db310
mecarm <melecarmine99@gmail.com>
2022-12-02 15:13:10 +0100

    importo font awesome e creo la topbar nell'header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    
-  </div>
+  <header>
+    <div class="top-bar">
+      <div class="content-top-bar">
+        <div>
+          <span><font-awesome-icon icon="fa-solid fa-phone" /></span>
+          <span>(001)88451234</span>
+          <span>88455438</span>
+        </div>
+        <div>
+          <span><font-awesome-icon icon="fa-brands fa-facebook-f" /></span>
+          <span><font-awesome-icon icon="fa-brands fa-twitter" /></span>
+          <span><font-awesome-icon icon="fa-brands fa-pinterest-p" /></span>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -12,5 +25,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../style/style.scss';
 
+.top-bar {
+  background-color: #3D3D3D;
+  padding: 5px 0;
+  font-size: 0.8rem;
+  font-weight: bold;
+
+  .content-top-bar {
+      @include flex($justify: Between_Center);
+      @include center;
+      color: white;
+
+      span{
+        margin-right: 10px;
+      }
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//FONTAWESOME 6
+
+  /* import the fontawesome core */
+  import { library } from '@fortawesome/fontawesome-svg-core'
+
+  /* import font awesome icon component */
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  /* import specific icons */
+  import { faUserSecret as faUserSecretSolid, faPhone as faPhoneSolid } from '@fortawesome/free-solid-svg-icons'
+  /*Import icone regular */
+  // import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
+
+  /* IMPORT BRAND ICON */
+   import { faFacebookF, faTwitter, faPinterestP } from '@fortawesome/free-brands-svg-icons'
+  /* add icons to the library */
+  library.add(faUserSecretSolid,faPhoneSolid, faFacebookF, faTwitter, faPinterestP)
+
+  /* add font awesome icon component */
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
 
 new Vue({
