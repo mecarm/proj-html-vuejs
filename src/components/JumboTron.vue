f532080556262539158f534541d29419086493c7
mecarm <melecarmine99@gmail.com>
2022-12-03 15:54:20 +0100

    Creo jumbotron layout

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
-    <div>
-        <H1>FOCUS ON YOUR BUSINESS</H1>
+    <div class="jumbo-description">
+        <h5>17 YEARS OF EXPIRIENCE</h5>
+        <h1>Focus on Your <span>Business</span> </h1>
     </div>
     <div class="image-jumbotron-1">
         <div class="image-jumbotron-2">
@@ -19,26 +20,46 @@ export default {
 
 <style lang="scss" scoped>
 @import '../style/style.scss';
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&family=Varela+Round&display=swap');
 
 section{
     @include flex($justify: 'flex');
+    background-color: #F5F5F5;
+
 }
+
+.jumbo-description {
+    margin-left: 10%;
+    padding-top: 130px;
+
+    h1 {
+        font-family: 'Varela Round', sans-serif;
+        font-size: 3.8rem;
+        color: black;
+        padding-top: 20px;
+
+        span{
+            color: #00D9A6;
+        }
+    }
+
+    h5 {
+        letter-spacing: 8px;
+        color: #00D9A6;
+        padding-left: 5px;
+    }
+}
+
 .image-jumbotron-1{
-    position: relative;
     height: 80vh;
-    width: 70%;
+    width: 100%;
 }
 .image-jumbotron-2{
-    position: absolute;
-    right: -35%;
     background-image: url("../assets/images/Group-35-2x.png");
-    background-size: cover;
-    background-position: center;
+    background-size: 120%;
+    background-position: left;
     background-repeat: no-repeat;        
     width: 100%;
-    height: 100%
+    height: 100%;
 }
-
-
-
 </style>
