cf5e084fce737076b7c96e4d6bb6fb91dfb3f92f
mecarm <melecarmine99@gmail.com>
2022-12-03 16:48:57 +0100

    Creo nuovo componente section nel main

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <JumboTron/>
+    <FirstSection/>
   </main>
 </template>
 
 <script>
 import JumboTron from './JumboTron.vue'
+import FirstSection from './FirstSection.vue'
 
 export default {
     name: 'MainComp',
     components: {
-        JumboTron
+        JumboTron,
+        FirstSection
     }
 }
 </script>
