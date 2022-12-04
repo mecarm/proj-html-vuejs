286da966ce804027dc03353f5ab15aa5d18d9e26
mecarm <melecarmine99@gmail.com>
2022-12-04 20:49:17 +0100

    Creazione componente ThirdSection

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -3,6 +3,7 @@
     <JumboTron/>
     <FirstSection/>
     <SecondSection/>
+    <ThirdSection/>
   </main>
 </template>
 
@@ -10,13 +11,15 @@
 import JumboTron from './JumboTron.vue'
 import FirstSection from './FirstSection.vue'
 import SecondSection from './SecondSection.vue'
+import ThirdSection from './ThirdSection.vue'
 
 export default {
     name: 'MainComp',
     components: {
         JumboTron,
         FirstSection,
-        SecondSection
+        SecondSection,
+        ThirdSection
     }
 }
 </script>
